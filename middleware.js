2e2b68533b06d754853be7274ad760f9fd258e48
Sofbritt <Sofi.harutyunyan23@gmail.com>
2023-03-31 16:00:24 +0400

    Redirect,Query,Prams

--- middleware.js
+++ middleware.js
@@ -1,26 +1,26 @@
 /////////// Middleware Homework ///////////
 
 
-const express = require('express')
-const app = express()
-const fs = require('fs')
+// const express = require('express')
+// const app = express()
+// const fs = require('fs')
 
 
 
-app.use((request, response, next) => {
+// app.use((request, response, next) => {
 
-    fs.readFile('count.txt', 'utf8', (err, data) => {
-        let count = data + 1
-        fs.writeFile('count.txt', `${count}`, (err) => {
-            (err) ? console.log(err) : console.log(err);
-        })
-        next()
-    })
+//     fs.readFile('count.txt', 'utf8', (err, data) => {
+//         let count = +data + 1
+//         fs.writeFile('count.txt', `${count}`, (err) => {
+//             (err) ? console.log(err) : console.log(err);
+//             next()
+//         })
+//     })
 
     
-})
+// })
 
 
-app.listen(3000, (err) => {
-    console.log('Listening port 3000');
-})
+// app.listen(3000, (err) => {
+//     console.log('Listening port 3000');
+// })
--- node1.js
+++ node1.js
@@ -1,22 +1,62 @@
+// const express = require('express')
+// const app = express()
+
+// app.use(express.static(__dirname + '/static'))
+
+
+// app.get('/', function (request, response) {
+//     response.sendFile(__dirname + '/html/index.html')
+
+// })
+
+// app.use((req, res) => {
+//     res.sendFile(__dirname + '/html/error.html')
+
+// })
+
+// app.listen(4000, (err) => {
+//     console.log('Listening port 4000');
+
+
+// })
+
+
+
+
+/////////////////////////
+
+
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/static'))
+// app.use((req, res, next) => {
+//     if (req.url === '/') {
+//         return res.send('not allowed')
+//     }
+//     next()
+// })
 
+// app.get('/:id',(req,res)=>{
+//     console.log(req.params);
+//     console.log(req.query);
+//     console.log(req.url);
+//     res.send('hello')
+// })
 
-app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/html/index.html')
+// 
+// app.get('/', (req, res) => {
+//     console.log(req.params);
+//     res.send('hello')
+// })
 
+app.get('/', (req, res) => {
+    res.send('home page')
 })
 
-app.use((req, res) => {
-    res.sendFile(__dirname + '/html/error.html')
-
+app.get('/about', (req, res) => {
+    res.redirect('/')
 })
 
-app.listen(4000, (err) => {
-    console.log('Listening port 4000');
-
-
+app.listen(4000, (error) => {
+    console.log(`Listening port ${4000}`);
 })
-
