6ba8821c1590a24975bc59cbe9983828b948bca3
Sofi <sofi.harutyunyan23@gmail.com>
2023-04-03 08:39:29 +0400

    Homework-Params

--- node1.js
+++ node1.js
@@ -26,8 +26,8 @@
 /////////////////////////
 
 
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
 // app.use((req, res, next) => {
 //     if (req.url === '/') {
@@ -43,20 +43,39 @@ const app = express()
 //     res.send('hello')
 // })
 
-// 
+//
 // app.get('/', (req, res) => {
 //     console.log(req.params);
 //     res.send('hello')
 // })
 
-app.get('/', (req, res) => {
-    res.send('home page')
+// app.get('/', (req, res) => {
+//     res.send('home page')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.redirect('/')
+// })
+
+// app.listen(4000, (error) => {
+//     console.log(`Listening port ${4000}`);
+// })
+
+
+
+//////////////  
+
+const express = require('express')
+const app = express()
+
+app.get('/:id', (req, res) => {
+    res.send('<h1>Welcome </h1>' + '</br>' + '<h3> URL </h3> ' + req.url + '</br>' + ' <h3>query</h3>' + req.query + '</br>' + '<h3>id</h3>' + req.id)
+    console.log(req.params);
+    console.log(req.query);
+
 })
 
-app.get('/about', (req, res) => {
-    res.redirect('/')
-})
 
-app.listen(4000, (error) => {
-    console.log(`Listening port ${4000}`);
+app.listen(3000, (err) => {
+    console.log(`Listening port ${3000}`);
 })
