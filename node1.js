728a8e18c753a84b2eac31e1c3f33f3b3d793d88
Sofi <sofi.harutyunyan23@gmail.com>
2023-04-05 12:25:36 +0400

    Post homework

--- node1.js
+++ node1.js
@@ -70,9 +70,14 @@
 
 // app.get('/:id', (req, res) => {
 
+
 //     console.log(req.params.id);
 //     console.log(req.query);
-//     res.send('<h1>Welcome </h1>' + '</br>' + '<h3> URL </h3> ' + req.url + '</br>' + ' <h3>query</h3>' + 'name:' + req.query.name+ '<br>' + 'age:' + req.query.age)
+
+//     res.send('<h1>Welcome </h1>' + '</br>' + '<h3> URL </h3> ' + req.url + '<br>' + ' <h3>query</h3>' + 'movie:' + req.query.movie + '<br>' + '</hr>' + 'year:' + req.query.year)
+//     console.log(req.params);
+//     console.log(req.query);
+
 
 // })
 
@@ -86,27 +91,94 @@
 
 
 
-//////////    POST   /////////
+
+
+
+
+
+
+
+
+
+
+///////////////////////// -----    POST -----  ///////////////////////
+
+// const express = require('express')
+// const app = express()
+
+// app.use(express.urlencoded())
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/html/index2.html')
+// })
+
+// app.post('/sub', (req, res) => {
+//     console.log(req.body);
+//     res.send('Data posted')
+
+//     // res.redirect('/')
+// })
+
+
+
+
+// app.listen(4000, (err) => {
+//     err ? console.log(err) : console.log('Listening port 4000');;
+// })
+
+
+
+
+
+
+
+////////// ------  POST   Homework ----- ////////
 
 const express = require('express')
 const app = express()
 
 app.use(express.urlencoded())
 
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/html/index2.html')
+
+    app.use(express.static(__dirname + '/static'))
+
+
+
 })
 
 app.post('/sub', (req, res) => {
     console.log(req.body);
-    // res.send('Data posted')
-    if (req.body.userName === '') {
-        return res.status(400).send('Wrong form')
-    }
-    res.redirect('/')
 
-})
+    if (req.body.userName === '') {
+        return res.status(400).send('Write your Name')
+
+    }
+
+    else if (req.body.mail === '') {
+        return res.status(400).send('Write your E-mail')
+    }
+
+    else {
+        res.send('Data posted' + '<br>' + 'user Name:' + req.body.userName + '<br>' + 'Area:' + req.body.select + '<br>' + 'Adress' + req.body.adresses + '<br>' + 'City:' + req.body.city + '<br>' + 'State:' + req.body.state + '<br>' + 'Country:' + req.body.country + '<br>' + 'e-mail:' + req.body.mail)
+
+    }
+
+}
+
+
+
+)
 
 app.listen(4000, (err) => {
-    err ? console.log(err) : console.log('Listening port 4000');;
+    err ? console.log(err) : console.log('Listening port 4000');
+    console.log('sffjsj');
+
+
+
 })
+
+
+
